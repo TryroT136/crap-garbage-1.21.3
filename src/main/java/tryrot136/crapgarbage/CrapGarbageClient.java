47a661becf866e5cf8432008b3454aeb04af8e21
TryroT136 <tieg.abercrombie@gmail.com>
2024-11-22 20:40:58 -0700

    a

---
+++ src/main/java/tryrot136/crapgarbage/CrapGarbageClient.java
@@ -0,0 +1,10 @@
+package tryrot136.crapgarbage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrapGarbageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/tryrot136/crapgarbage/CrapGarbageDataGenerator.java
@@ -0,0 +1,11 @@
+package tryrot136.crapgarbage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrapGarbageDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
